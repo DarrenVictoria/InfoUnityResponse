5af6820bf75d851ebbe84ffd832b6cba548b78fc
DarrenVictoria <darrenvictoria2@gmail.com>
2024-11-05 22:47:00 +0530

    Full page background fix
    Full page background fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c9deea91'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g1j7on8mh4o"
+    "revision": "0.2605f30tmp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
